8b62d499a556b50a497f8a7f27d4dc80704d3ab8
ayman <aymanhanyibrahimkhalel@gmail.com>
2019-10-21 11:15:20 +0200

    first push

--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -12,43 +12,4 @@ $(document).ready(function() {
     }
 
     // Slick Plugin 
-
-    $('.slider').slick({
-        dots: false,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows:false,
-        autoplay: true,
-        autoplaySpeed: 1000,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              infinite: true,
-              dots: false
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
-        ]
-    });
 });
